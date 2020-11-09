7d0385e9288c98cb9c10c0994eabba42a6f50e98
Everton Souza <evrasouza@gmail.com>
2020-11-09 07:08:04 -0300

    Retornando uma lista

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ get "/" do
   content_type "application/json"
   return { message: "Welcome to book Api from QA Ninja!" }.to_json
 end
+
+get "/books" do
+  content_type "application/json"
+  books = [
+    { title: "Dom Casmurro", author: "Machado de Assis", isbn: "abd2001" },
+    { title: "A marca de uma lágrima", author: "Pedro Bandeira", isbn: "abd2002" },
+    { title: "Moby Dick", author: "Hernan Melville", isbn: "abd2003" },
+  ]
+  return books.to_json
+end
